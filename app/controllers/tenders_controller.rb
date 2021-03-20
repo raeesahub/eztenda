fc2c06829537ee7da46b55e723777ca8ddb6a802
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-03-20 21:19:12 +0000

    added select buttons with styling

--- app/controllers/tenders_controller.rb
+++ app/controllers/tenders_controller.rb
@@ -2,6 +2,10 @@ class TendersController < ApplicationController
   def index
     if params[:query].present?
       @tenders = Tender.search(params[:query])
+    elsif params[:category].present?
+      category_id = params[:category].to_i
+      y = Tender.where(categories_id: category_id).pluck(:tender_id).uniq
+      @tenders = y
     else
       @tenders = Tender.all
     end
