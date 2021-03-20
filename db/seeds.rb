55c7b012dd856267c9f4ed97bbe2a316636b882a
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-03-20 19:06:05 +0000

    added category ids for tenders

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,8 +51,9 @@ tender1 = Tender.create!(
   volume: "1000",
   active: true,
   min_bid: "£1000",
-  business_id: be_at_one.id
-  )
+  business_id: be_at_one.id,
+  categories_id: Category.find_by(description: "gin").id
+)
 
 tender2 = Tender.create!(
   description: "Tonic Water",
@@ -61,7 +62,8 @@ tender2 = Tender.create!(
   volume: "2500",
   active: true,
   min_bid: "£750",
-  business_id: ping_pong.id
+  business_id: ping_pong.id,
+  categories_id: Category.find_by(description: "tonic water").id
   )
 
 tender3 = Tender.create!(
@@ -71,7 +73,8 @@ tender3 = Tender.create!(
   volume: "500",
   active: true,
   min_bid: "£300",
-  business_id: eat_tokyo.id
+  business_id: eat_tokyo.id,
+  categories_id: Category.find_by(description: "sake").id
   )
 
 tender4 = Tender.create!(
@@ -81,7 +84,8 @@ tender4 = Tender.create!(
   volume: "900",
   active: true,
   min_bid: "£500",
-  business_id: las_iguanas.id
+  business_id: las_iguanas.id,
+  categories_id: Category.find_by(description: "tequila").id
   )
 
 bid1 = Bid.create!(
