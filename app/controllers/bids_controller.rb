fab6b828478c7bf06d06812b0a7ddacd90170ce0
Felix <FelixinLondon@googlemail.com>
2021-03-20 17:04:48 +0100

    Added bidindex

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,2 +1,7 @@
 class BidsController < ApplicationController
+    def index
+        @user = User.find(current_user.id)
+        @bids = Bid.where(user_id: @user.id)
+        return @bids
+    end
 end
--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,5 +1,5 @@
 class Bid < ApplicationRecord
     belongs_to :tender,foreign_key: "tender_id"
-    belongs_to :product,foreign_key: "product_id"
-    belongs_to :user, :through => :products
+    belongs_to :product,foreign_key: "product_id",optional: true
+    belongs_to :user
 end
