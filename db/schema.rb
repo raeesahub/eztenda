a76900800b59317aa616bd8342837425fc9fd052
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-03-21 10:07:04 +0000

    added legal name to users and fixed devise params

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_111645) do
+ActiveRecord::Schema.define(version: 2021_03_21_095134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_03_20_111645) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "user_type"
+    t.string "legal_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
